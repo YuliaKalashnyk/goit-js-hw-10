794b5a31123d7b8728638e6df28ffcc9b80c1220
Yulia Kalashnyk <jkalashnik.97@gmail.com>
2023-04-21 20:37:10 +0300

    com3

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,5 @@
 const URL_NAME = 'https://restcountries.com/v3.1/name/';
-const FIELDS = 'name,capital,population,flags,languages';
+const FIELDS = 'name.official,capital,population,flags.svg,languages';
 
 export function fetchCountries(name) {
   return fetch(`${URL_NAME}${name}?fields=${FIELDS}`).then(response => {
